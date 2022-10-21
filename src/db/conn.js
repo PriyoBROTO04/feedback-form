738c117b3de249bf9cd638153637f83c3ec5b5e2
Priyobroto Kar <priyobrotokar2004@gmail.com>
2022-10-21 23:24:44 +0530

    mongo

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/discordFeedbackForm",{
+mongoose.connect("mongodb+srv://priyobroto:<password>@cluster0.5fojxl0.mongodb.net/test",{
     useNewUrlParser : true,
     useUnifiedTopology : true
 }).then(()=>{
