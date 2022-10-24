2b7cae2d12d1e95725156462088b7ed5727df0d5
Priyobroto Kar <priyobrotokar2004@gmail.com>
2022-10-24 18:15:12 +0530

    connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb+srv://priyobroto:Sonu1234?@cluster0.5fojxl0.mongodb.net/feedbackform?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://priyobroto:priyobroto@cluster0.5fojxl0.mongodb.net/feedbackform?retryWrites=true&w=majority",{
     useNewUrlParser : true,
     useUnifiedTopology : true
 }).then(()=>{
