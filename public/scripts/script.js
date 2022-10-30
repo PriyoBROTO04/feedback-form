87eb2b85643f426a796b241c4eda6b3f54a8cc24
Priyobroto Kar <priyobrotokar2004@gmail.com>
2022-10-30 09:02:51 +0530

    final

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,5 @@
 const enterbtn = document.getElementById("enterbtn");
+const form=document.getElementById('form');
 const intro = document.querySelector(".intro");
 const gen = document.querySelector(".gen");
 const refer = document.querySelector(".refer");
@@ -17,11 +18,13 @@ const referinput = refer.querySelectorAll("textarea");
 const infoinput = info.querySelectorAll("input");
 
 
+
+
 enterbtn.addEventListener("click", () => {
     intro.classList.remove('cardactive');
     gen.classList.add('cardactive');
-    tabs.classList.add('tabsactive');
-    gentab.classList.add('tabactive');
+    // tabs.classList.add('tabsactive');
+    // gentab.classList.add('tabactive');
 
 })
 gennxtbtn.addEventListener("click", () => {
@@ -31,8 +34,8 @@ gennxtbtn.addEventListener("click", () => {
             gen.classList.remove('cardactive');
             gen.classList.add('carddeactive');
             refer.classList.add('cardactive');
-            gentab.classList.remove('tabactive');
-            refertab.classList.add('tabactive');
+            // gentab.classList.remove('tabactive');
+            // refertab.classList.add('tabactive');
         }
         else {
             input.classList.add('error');
@@ -54,40 +57,49 @@ genbckbtn.addEventListener("click", () => {
 
 
 refernxtbtn.addEventListener("click", () => {
-    let f = 0;
     referinput.forEach(input => {
-
-
-        if (input.value != "") {
-            f = f + 1;
-
+        {
+            input.classList.remove('error');
         }
 
-
     })
-    console.log(f);
-    if (f >= 2) {
-        refer.classList.remove('cardactive');
-        refer.classList.add('carddeactive');
-        info.classList.add('cardactive');
-        // refertab.classList.remove('tabactive');
-        // infotab.classList.add('tabactive');
+    let f = 0;
+    setTimeout(function checkrefer() {
         referinput.forEach(input => {
-            if(input.value!=""){
-                input.classList.remove('error');
-            }
-            
-        })
 
-    }
-    else {
-        referinput.forEach(input => {
-            if(input.value==""){
-                input.classList.add('error')
+
+            if (input.value != "") {
+                f = f + 1;
+
             }
-            
+
+
         })
-    }
+        console.log(f);
+        if (f >= 2) {
+            refer.classList.remove('cardactive');
+            refer.classList.add('carddeactive');
+            info.classList.add('cardactive');
+            // refertab.classList.remove('tabactive');
+            // infotab.classList.add('tabactive');
+            referinput.forEach(input => {
+                if (input.value != "") {
+                    input.classList.remove('error');
+                }
+
+            })
+
+        }
+        else {
+            referinput.forEach(input => {
+                if (input.value == "") {
+                    input.classList.add('error')
+                }
+
+            })
+        }
+    }, 1)
+
 })
 referbckbtn.addEventListener("click", () => {
     refer.classList.remove('cardactive');
@@ -106,39 +118,220 @@ infobckbtn.addEventListener("click", () => {
 
 })
 
-function validateform() {
+function validateform(){
+    var id = document.getElementById("ID");
+    var username = document.getElementById("username");
+    let index = username.value.length - 5;
+    id.classList.remove('error');
+    username.classList.remove('error');
     let f = 0;
-    infoinput.forEach(input => {
-        if (input.value != "") {
-            f = f + 1;
-
+    setTimeout(()=>{
+        if (id.value.length == 18) {
+            id.classList.remove('error');
         }
-        
-    })
-    console.log(f);
-    if(f==2){
-        infoinput.forEach(input => {
-            if(input.value==""){
-                input.classList.remove('error')
-            }
-            
-        })
-        
-        
-        return true;
-    }
-    else{
-        infoinput.forEach(input => {
-            if(input.value==""){
-                input.classList.add('error')
-            }
-            
-        })
-        return false;
-    }
+        else {
+                 id.classList.add('error');
+                    
+        }
+        if (username.value.charAt(index) == '#') {
+            username.classList.remove('error');
+        }
+        else {
+             username.classList.add('error');
+        }},5)
+
+
+        if (id.value.length == 18) {
+            f++;
+        }
+        if (username.value.charAt(index) == '#') {
+            f++;
+        }
+        console.log(f);
+        if(f==2){
+            return true;
+        }
+        else{
+            return false;
+        }
     
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// form.addEventListener('submit',(e)=>{
+//     var id = document.getElementById("ID");
+//     var username = document.getElementById("username");
+//     let index = username.value.length - 5;
+//     id.classList.remove('error');
+//     username.classList.remove('error');
+//     let f = 0;
+//     async function validation(){
+//         let check=new Promise((resolve,reject)=>{
+//             setTimeout(()=>{
+//                 if (id.value.length == 18) {
+//                         id.classList.remove('error');
+//                         f++;
+
+            
+            
+            
+            
+            
+            
+//                     }
+//                     else {
+//                         id.classList.add('error');
+            
+//                     }
+//             if (username.value.charAt(index) == '#') {
+//                         username.classList.remove('error');
+//                         f++;
+//                         resolve(f);
+    
+            
+            
+            
+            
+            
+            
+//                     }
+//                     else {
+//                         username.classList.add('error');
+    
+            
+//                     }},100)
+//                 })
+//                 let t=await check;
+//                 return t;
+
+//     }
+//     let a=validation();
+//     a.then((value)=>{
+//         console.log(value);
+//         if(value!=2){
+//             e.preventDefault();
+            
+//         }
+    
+//         })
+        
+
+
+    
+    
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function validateform() {
+//     var id = document.getElementById("ID");
+//     var username = document.getElementById("username");
+//     let index = username.value.length - 5;
+//     id.classList.remove('error');
+//     username.classList.remove('error');
+//     let f = 0;
+//     let check=new Promise((resolve ,reject)=>{
+//         setTimeout(function check() {
+//         if (id.value.length == 18) {
+//             id.classList.remove('error');
+//             f++;
+//             console.log(f);
+
+
+
+
+
+
+//         }
+//         else {
+//             id.classList.add('error');
+
+//         }
+//         if (username.value.charAt(index) == '#') {
+//             username.classList.remove('error');
+//             f++;
+//             console.log(f);
+//             return false;
+
+
+
+
+
+
+//         }
+//         else {
+//             username.classList.add('error');
+//             return false;
+
+//         }
+        
+
+//     }, 50)
+// })
+
+
+
+//     // setTimeout(function submit(){
+//     //     console.log(f);
+
+//     //     if (f == 2) {
+//     //         return false;
+//     //     }
+//     //     else {
+//     //         return false
+//     //     }
+//     // })
+//     let t=await check;
+//     alert(t);
+//     return false;
+
+
+// }
+
+
+
+
 
 
 
